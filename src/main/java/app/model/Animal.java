cdf53ded41399802efad6435555fd405c7b5cfea
Никита <godunov987@mail.ru>
2022-07-12 16:32:09 +0300

    animals

--- src/main/java/app/model/Animal.java
+++ src/main/java/app/model/Animal.java
@@ -1,4 +1,13 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public abstract class Animal {
+
+
 }
+
+
